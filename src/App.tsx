22b3e84a324c7d12ddb10b13a93fb6c491c1da5b
rafaelcastrobr <rafaelcastrobr@gmail.com>
2022-07-12 16:51:06 -0300

    update e criação list Item com typescript

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,30 @@ import { useState } from 'react';
 import * as C from './App.styles'
 import { Item } from './types/Item'
 import { ListItem } from './components/ListItem/ListItem';
-
+import { AddArea } from './components/AddArea/AddArea'
 
 const App = () => {
   const [list, setList] = useState<Item[]>([
     { id: 1, name: 'Comprar pão', done: false},
-    { id: 2, name: 'Comprar bolo', done: false},
-
+    { id: 2, name: 'Comprar bolo', done: true},
   ])
+
+  const handleAddTask = (taskName: string) => {
+    let newList = [...list];
+    newList.push({
+      id: list.length+1,
+      name: taskName, 
+      done: false
+    })
+    setList(newList);
+  }
+
   return (
     <C.Container>
       <C.Area>
         <C.Header>Lista de Tarefas</C.Header>
 
-        {/* Área de adc nova tarefa */}
+        <AddArea onEnter={handleAddTask}/>
 
         {list.map((item, index) => (
           <ListItem key={index} item={item}/>
--- src/components/ListItem/ListItem.tsx
+++ src/components/ListItem/ListItem.tsx
@@ -1,14 +1,22 @@
-import * as C from './ListItemStyle'
+import * as C from './ListItem.styles'
 import { Item } from '../../types/Item'
+import { KeyboardEvent, useState } from 'react'
 
 type Props = {
   item: Item
 }
 
-export const ListItem = ({ item}: Props) => {
+export const ListItem = ({ item }: Props) => {
+  const [isChecked, setIsChecked] = useState(item.done)
+
+  const handleIsChecked = (e: any) => {
+    item.done = e.target.checked
+  }
+
   return (
-    <C.Container>
-      {item.name}
+    <C.Container done={isChecked}>
+      <input type="checkbox" checked={isChecked} onChange={e => setIsChecked(e.target.checked)} onClick={e => handleIsChecked(e)}/>
+      <label>{item.name} - {item.done.toString()}</label>
     </C.Container>
   )
 }
